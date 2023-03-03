86d5737812fa6e3745a0ac1e5eaa55b008357c8d
Brian Berns <brianberns@gmail.com>
2023-03-03 14:01:50 -0500

    Work on monitoring replies.

--- RedditChatBot/Program.fs
+++ RedditChatBot/Program.fs
@@ -1,22 +1,40 @@
 ﻿namespace RedditChatBot
 
 open System
+open Reddit.Controllers
 
 module Program =
 
+    let reply (comment : Comment) =
+
+            // have I already replied to this comment?
+        let handled =
+            comment.Comments.GetComments()
+                |> Seq.exists (fun child ->
+                    child.Author = "EverydayChatBot")
+
+            // if not, create a reply
+        if not handled then
+            (*
+            let reply = Chat.chat comment.Body
+            printfn $"{reply}"
+            comment.Reply(reply) |> ignore
+            *)
+            ()
+
     [<EntryPoint>]
     let main args =
-        use _ =
-            let post = Reddit.client.Post("t3_11glnkd")   // "I am a ChatGPT bot"
-            Reddit.monitor post (fun comment ->
-                let handled =
-                    comment.Comments.GetComments()
-                        |> Seq.exists (fun child ->
-                            child.Author = "EverydayChatBot")
-                if not handled then
-                    Chat.chat comment.Body
-                        |> comment.Reply
-                        |> ignore)
+
+            // reply to top-level comments made on my posts
+        let me = Reddit.client.Account.Me
+        (*
+        Reddit.monitorUserPosts me (fun post ->
+            Reddit.monitorCommentStream post.Comments reply)
+        *)
+
+            // reply to comments made to my comments
+        Reddit.monitorUserComments me (fun comment ->
+            Reddit.monitorCommentStream comment.Comments reply)
 
         Console.ReadLine() |> ignore
         0
--- RedditChatBot/Reddit.fs
+++ RedditChatBot/Reddit.fs
@@ -1,7 +1,9 @@
 ﻿namespace RedditChatBot
 
 open System
+
 open Reddit
+open Reddit.Controllers
 
 module Reddit =
 
@@ -13,18 +15,35 @@ module Reddit =
             refreshToken = settings.RefreshToken,
             appSecret = settings.AppSecret)
 
-    let monitor (post : Controllers.Post) callback =
+    /// Monitors for new posts by the given user.
+    let monitorUserPosts (user : User) callback =
 
-        let flag = post.Comments.MonitorNew()
+        let flag = user.MonitorPostHistory()
         assert(flag)
 
-        post.Comments.NewUpdated.Add(fun evt ->
+        user.PostHistoryUpdated.Add(fun evt ->
+            for post in evt.Added do
+                printfn $"monitorUserPosts: {post.Title}"
+                callback post)
+
+    /// Monitors for new comments by the given user.
+    let monitorUserComments (user : User) callback =
+
+        let flag = user.MonitorCommentHistory()
+        assert(flag)
+
+        user.CommentHistoryUpdated.Add(fun evt ->
             for comment in evt.Added do
+                printfn $"monitorUserComments: {comment.Body}"
                 callback comment)
 
-        {
-            new IDisposable with
-                member _.Dispose() =
-                    let flag = post.Comments.MonitorNew()
-                    assert(not flag)
-        }
+    /// Monitor for new comments in the given stream.
+    let monitorCommentStream (stream : Comments) callback =
+
+        let flag = stream.MonitorNew()
+        assert(flag)
+
+        stream.NewUpdated.Add(fun evt ->
+            for comment in evt.Added do
+                printfn $"monitorCommentStream: {comment.Body}"
+                callback comment)
