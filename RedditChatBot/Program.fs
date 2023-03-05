7cb5cbacf06adc87ff70c5673b8c6e54ed18ccca
Brian Berns <brianberns@gmail.com>
2023-03-05 00:07:34 -0500

    Avoid monitoring and other janky API behavior.

--- RedditChatBot/Program.fs
+++ RedditChatBot/Program.fs
@@ -1,40 +1,48 @@
 ﻿namespace RedditChatBot
 
-open System
 open Reddit.Controllers
 
 module Program =
 
-    let reply (comment : Comment) =
+    let userName = "EverydayChatBot"
 
-            // have I already replied to this comment?
-        let handled =
-            comment.Comments.GetComments()
-                |> Seq.exists (fun child ->
-                    child.Author = "EverydayChatBot")
+    /// Replies to the given comment, if necessary.
+    let private reply (comment : Comment) =
 
-            // if not, create a reply
-        if not handled then
-            (*
-            let reply = Chat.chat comment.Body
-            printfn $"{reply}"
-            comment.Reply(reply) |> ignore
-            *)
-            ()
+        let comment = comment.About()             // get full properties
+
+            // ignore my own comments
+        if comment.Author <> userName
+            && comment.Depth < 5                  // avoid infinite recursion with another bot
+            && comment.Body <> "[deleted]" then   // no better way to check this?
+
+                // have I already replied to this comment?
+            let handled =
+                comment.Replies
+                    |> Seq.exists (fun child ->
+                        child.Author = userName)
+
+                // if not, create a reply
+            if not handled then
+                printfn ""
+                printfn $"User says: {comment.Body}"
+                let replyBody = Chat.chat comment.Body
+                printfn $"I say: {replyBody}"
+                comment.Reply(replyBody) |> ignore
 
     [<EntryPoint>]
     let main args =
 
-            // reply to top-level comments made on my posts
-        let me = Reddit.client.Account.Me
-        (*
-        Reddit.monitorUserPosts me (fun post ->
-            Reddit.monitorCommentStream post.Comments reply)
-        *)
+        let me = Reddit.client.User(userName)
+           
+            // reply to any top-level comments in my posts
+        for post in me.PostHistory do
+            for comment in post.Comments.GetNew() do
+                reply comment
 
-            // reply to comments made to my comments
-        Reddit.monitorUserComments me (fun comment ->
-            Reddit.monitorCommentStream comment.Comments reply)
+            // reply to replies to my recent comments
+        for myComment in me.CommentHistory do
+            for comment in myComment.About().Replies do
+                reply comment
 
-        Console.ReadLine() |> ignore
         0
--- RedditChatBot/Reddit.fs
+++ RedditChatBot/Reddit.fs
@@ -1,9 +1,6 @@
 ﻿namespace RedditChatBot
 
-open System
-
 open Reddit
-open Reddit.Controllers
 
 module Reddit =
 
@@ -14,36 +11,3 @@ module Reddit =
             appId = "fVstFww14kdp4hFRJCCzdg",
             refreshToken = settings.RefreshToken,
             appSecret = settings.AppSecret)
-
-    /// Monitors for new posts by the given user.
-    let monitorUserPosts (user : User) callback =
-
-        let flag = user.MonitorPostHistory()
-        assert(flag)
-
-        user.PostHistoryUpdated.Add(fun evt ->
-            for post in evt.Added do
-                printfn $"monitorUserPosts: {post.Title}"
-                callback post)
-
-    /// Monitors for new comments by the given user.
-    let monitorUserComments (user : User) callback =
-
-        let flag = user.MonitorCommentHistory()
-        assert(flag)
-
-        user.CommentHistoryUpdated.Add(fun evt ->
-            for comment in evt.Added do
-                printfn $"monitorUserComments: {comment.Body}"
-                callback comment)
-
-    /// Monitor for new comments in the given stream.
-    let monitorCommentStream (stream : Comments) callback =
-
-        let flag = stream.MonitorNew()
-        assert(flag)
-
-        stream.NewUpdated.Add(fun evt ->
-            for comment in evt.Added do
-                printfn $"monitorCommentStream: {comment.Body}"
-                callback comment)
