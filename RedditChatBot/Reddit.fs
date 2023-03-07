cc47c365823db214abe4120d5ce342409aa76888
Brian Berns <brianberns@gmail.com>
2023-03-07 13:08:45 -0500

    Commented.

--- RedditChatBot/Reddit.fs
+++ RedditChatBot/Reddit.fs
@@ -4,11 +4,13 @@ open Reddit
 
 module Reddit =
 
+    /// Reddit application settings.
     let private settings = Settings.get.Reddit
 
+    /// Reddit client.
     let client =
         RedditClient(
-            appId = "fVstFww14kdp4hFRJCCzdg",
+            appId = "fVstFww14kdp4hFRJCCzdg",                      // no harm in sharing this
             refreshToken = settings.RefreshToken,
             appSecret = settings.AppSecret,
-            userAgent = "RedditChatBot:v1.0 (by /u/brianberns)")
+            userAgent = "RedditChatBot:v1.0 (by /u/brianberns)")   // this is the format that Reddit suggests (more or less)
