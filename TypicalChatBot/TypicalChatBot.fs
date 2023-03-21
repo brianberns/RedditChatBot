937816eb4778cb85b46310c7fab61a6dd0696ada
Brian Berns <brianberns@gmail.com>
2023-03-21 09:40:13 -0400

    Added typical-chat-bot.

---
+++ TypicalChatBot/TypicalChatBot.fs
@@ -0,0 +1,31 @@
+﻿namespace RedditChatBot
+
+open Microsoft.Azure.WebJobs
+open Microsoft.Extensions.Configuration
+open Microsoft.Extensions.Logging
+
+/// Azure function type for dependency injection.
+type FriendlyChatBot(config : IConfiguration) =
+
+    /// Reply prompt.
+    let replyPrompt =
+        """
+You are a typical Reddit user. If you receive a comment
+that seems strange or irrelevant, do your best to play along.
+        """
+
+    /// Runs the bot.
+    [<FunctionName("MonitorUnreadMessages")>]
+    member _.Run(
+        [<TimerTrigger("0 * * * * *")>]   // every minute at second 0
+        timer : TimerInfo,
+        log : ILogger) =
+
+        let settings = config.Get<AppSettings>()
+        let botDesc =
+            BotDescription.create
+                "typical-chat-bot"
+                "1.0"
+                "brianberns"
+                replyPrompt
+        Bot.monitorUnreadMessages settings botDesc log
