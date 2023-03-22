82cd9eff26617e22f2cc180933e9174e23c489dd
Brian Berns <brianberns@gmail.com>
2023-03-22 00:10:32 -0400

    Added Reddit test.

---
+++ ChatTest/RedditTest.fs
@@ -0,0 +1,29 @@
+﻿namespace RedditChatBot
+
+open System
+open Microsoft.Extensions.Configuration
+
+module RedditTest =
+
+    let redditClient =
+
+        let settings =
+            let cs =
+                Environment.GetEnvironmentVariable("ConnectionString")
+            ConfigurationBuilder()
+                .AddAzureAppConfiguration(cs)
+                .Build()
+                .Get<AppSettings>()
+        let botDesc =
+            BotDescription.create
+                "friendly-chat-bot"
+                "1.0"
+                "brianberns"
+                ""
+        Reddit.createClient settings.Reddit botDesc
+
+    let test () =
+        let messages =
+            redditClient.Account.Messages
+                .GetMessagesUnread(limit = 1000)
+        printfn $"{messages.Count}"
