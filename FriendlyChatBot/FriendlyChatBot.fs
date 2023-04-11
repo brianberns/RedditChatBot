d191f618b87405966bed06a412d9870718e5d39c
Brian Berns <brianberns@gmail.com>
2023-04-11 12:24:44 -0400

    Fixed schedule.

--- FriendlyChatBot/FriendlyChatBot.fs
+++ FriendlyChatBot/FriendlyChatBot.fs
@@ -95,7 +95,7 @@ Specify the title with "Title:" and a one-sentence body with "Body:".
     /// Posts a random thought.
     [<FunctionName("PostRandomThought")>]
     member _.PostRandomThought(
-        [<TimerTrigger("*/3 * * * * *")>]   // every three hours
+        [<TimerTrigger("0 0 */3 * * *")>]   // every three hours
         timer : TimerInfo,
         log : ILogger) =
         createBot postPrompt log
