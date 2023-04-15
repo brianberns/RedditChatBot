1e4c1e634b902c177233b589205400f53e454c16
Brian Berns <brianberns@gmail.com>
2023-04-15 19:30:23 -0400

    Refactored.

--- FriendlyChatBot/FriendlyChatBot.fs
+++ FriendlyChatBot/FriendlyChatBot.fs
@@ -8,78 +8,14 @@ open Microsoft.Extensions.Logging
 
 open OpenAI.GPT3.ObjectModels
 
-/// Azure function type for dependency injection.
-type FriendlyChatBot(config : IConfiguration) =
-
-    /// Reply prompt.
-    let replyPrompt =
-        """
-You are a friendly Reddit user. If you receive a comment
-that seems strange or irrelevant, do your best to play along.
-        """
-
-    /// Random thought prompt.
-    let randomThoughtPrompt =
-        """
-Generate a random thought for the /r/RandomThoughts subreddit.
-Specify the title with "Title:" and a one-sentence body with
-"Body:".
-        """
-
-    /// Six word story prompt.
-    let sixWordStoryPrompt =
-        """
-Generate a six-word story for the /r/sixwordstories subreddit.
-        """
-
-    /// Crazy idea prompt.
-    let crazyIdeaPrompt =
-        """
-Generate a one-sentence crazy idea for the /r/CrazyIdeas subreddit.
-        """
-
-    /// Creates a bot.
-    let createBot prompt log =
-        let settings = config.Get<AppSettings>()
-        let redditBotDef =
-            RedditBotDef.create
-                "friendly-chat-bot"
-                "1.0"
-                "brianberns"
-        let chatBotDef =
-            ChatBotDef.create prompt Models.Gpt_4
-        let bot = Bot.create settings redditBotDef chatBotDef log
-        log.LogInformation("Bot initialized")
-        bot
-
-    /// Trims the given prefix from the given string.
-    let trimPrefix (prefix : string) (str : string) =
-        if str.StartsWith(prefix) then
-            str.Substring(prefix.Length).Trim()
-        else failwith $"Missing prefix \"{prefix}\" in \"{str}\""
-
-    /// Creates a random thought.
-    let createRandomThought bot =
-        let parts =
-            let completion = ChatBot.complete [] bot.ChatBot
-            completion.Split(
-                '\n',
-                StringSplitOptions.RemoveEmptyEntries)
-                |> Seq.map (fun part -> part.Trim())
-                |> Seq.toList
-        match parts with
-            | [ titlePart; bodyPart ] ->
-                let title = trimPrefix "Title:" titlePart
-                let body = trimPrefix "Body:" bodyPart
-                title, body
-            | _ -> failwith $"Unexpected number of parts: {parts}"
+module Post =
 
     /// # of retry attempts.
     let numTries = 3
 
     /// Submits a post
-    let submitPost subredditName title body bot =
-        Bot.tryN numTries (fun iTry ->
+    let submit subredditName title body bot =
+        Bot.tryN 3 (fun iTry ->
             try
                 let post =
                     let subreddit =
@@ -97,25 +33,97 @@ Generate a one-sentence crazy idea for the /r/CrazyIdeas subreddit.
                 Bot.handleException exn bot.Log
                 false, None)
 
+module CrazyIdea =
+
+    /// Crazy idea prompt.
+    let prompt =
+        """
+Generate a one-sentence crazy idea for the /r/CrazyIdeas subreddit.
+        """
+
+    /// Posts a crazy idea.
+    let post bot =
+        let title = ChatBot.complete [] bot.ChatBot
+        Post.submit "CrazyIdeas" title "" bot
+
+module RandomThought =
+
+    /// Random thought prompt.
+    let prompt =
+        """
+Generate a random thought for the /r/RandomThoughts subreddit.
+Specify the title with "Title:" and a one-sentence body with
+"Body:".
+        """
+
+    /// Trims the given prefix from the given string.
+    let private trimPrefix (prefix : string) (str : string) =
+        if str.StartsWith(prefix) then
+            str.Substring(prefix.Length).Trim()
+        else failwith $"Missing prefix \"{prefix}\" in \"{str}\""
+
+    /// Creates a random thought.
+    let private create bot =
+        let parts =
+            let completion = ChatBot.complete [] bot.ChatBot
+            completion.Split(
+                '\n',
+                StringSplitOptions.RemoveEmptyEntries)
+                |> Seq.map (fun part -> part.Trim())
+                |> Seq.toList
+        match parts with
+            | [ titlePart; bodyPart ] ->
+                let title = trimPrefix "Title:" titlePart
+                let body = trimPrefix "Body:" bodyPart
+                title, body
+            | _ -> failwith $"Unexpected number of parts: {parts}"
+
     /// Posts a random thought.
-    let postRandomThought bot =
-        let title, body = createRandomThought bot
-        submitPost "RandomThoughts" title body bot
+    let post bot =
+        let title, body = create bot
+        Post.submit "RandomThoughts" title body bot
+
+module SixWordStory =
+
+    /// Six-word story prompt.
+    let prompt =
+        """
+Generate a six-word story for the /r/sixwordstories subreddit.
+        """
 
     /// Posts a six word story.
-    let postSixWordStory bot =
-        Bot.tryN numTries (fun _ ->
+    let post bot =
+        Bot.tryN Post.numTries (fun _ ->
             let title = ChatBot.complete [] bot.ChatBot
             if title.Split(' ').Length = 6 then
-                true, submitPost "sixwordstories" title "" bot
+                true, Post.submit "sixwordstories" title "" bot
             else
                 bot.Log.LogError($"Not a six-word story: {title}")
                 false, None)
 
-    /// Posts a crazy idea.
-    let postCrazyIdea bot =
-        let title = ChatBot.complete [] bot.ChatBot
-        submitPost "CrazyIdeas" title "" bot
+/// Azure function type for dependency injection.
+type FriendlyChatBot(config : IConfiguration) =
+
+    /// Reply prompt.
+    let replyPrompt =
+        """
+You are a friendly Reddit user. If you receive a comment
+that seems strange or irrelevant, do your best to play along.
+        """
+
+    /// Creates a bot.
+    let createBot prompt log =
+        let settings = config.Get<AppSettings>()
+        let redditBotDef =
+            RedditBotDef.create
+                "friendly-chat-bot"
+                "1.0"
+                "brianberns"
+        let chatBotDef =
+            ChatBotDef.create prompt Models.Gpt_4
+        let bot = Bot.create settings redditBotDef chatBotDef log
+        log.LogInformation("Bot initialized")
+        bot
 
     /// Monitors unread messages.
     [<FunctionName("MonitorUnreadMessages")>]
@@ -127,14 +135,24 @@ Generate a one-sentence crazy idea for the /r/CrazyIdeas subreddit.
             |> Bot.monitorUnreadMessages
             |> ignore
 
+    /// Posts a crazy idea.
+    [<FunctionName("PostCrazyIdea")>]
+    member _.PostCrazyIdea(
+        [<TimerTrigger("0 15 1,13 * * *")>]   // twice a day at 01:15 and 13:15
+        timer : TimerInfo,
+        log : ILogger) =
+        createBot CrazyIdea.prompt log
+            |> CrazyIdea.post
+            |> ignore
+
     /// Posts a random thought.
     [<FunctionName("PostRandomThought")>]
     member _.PostRandomThought(
         [<TimerTrigger("0 15 0,12 * * *")>]   // twice a day at 00:15 and 12:15
         timer : TimerInfo,
         log : ILogger) =
-        createBot randomThoughtPrompt log
-            |> postRandomThought
+        createBot RandomThought.prompt log
+            |> RandomThought.post
             |> ignore
 
     /// Posts a six word story.
@@ -143,16 +161,6 @@ Generate a one-sentence crazy idea for the /r/CrazyIdeas subreddit.
         [<TimerTrigger("0 15 23 * * *")>]     // once a day at 23:15
         timer : TimerInfo,
         log : ILogger) =
-        createBot sixWordStoryPrompt log
-            |> postSixWordStory
-            |> ignore
-
-    /// Posts a crazy idea.
-    [<FunctionName("PostCrazyIdea")>]
-    member _.PostCrazyIdea(
-        [<TimerTrigger("0 15 1,13 * * *")>]   // twice a day at 01:15 and 13:15
-        timer : TimerInfo,
-        log : ILogger) =
-        createBot crazyIdeaPrompt log
-            |> postCrazyIdea
+        createBot SixWordStory.prompt log
+            |> SixWordStory.post
             |> ignore
