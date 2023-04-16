bc2598505796557223b4030cc861dfc86a6955d3
Brian Berns <brianberns@gmail.com>
2023-04-15 22:55:17 -0400

    Simplified.

--- ChatTest/ChatTest.fs
+++ ChatTest/ChatTest.fs
@@ -8,8 +8,7 @@ module ChatTest =
 
     let prompt =
         """
-You are a friendly Reddit user. If you receive a comment
-that seems strange or irrelevant, do your best to play along.
+Generate a one-sentence post for the /r/ShowerThoughts subreddit.
         """
 
     let chatBot =
@@ -26,33 +25,7 @@ that seems strange or irrelevant, do your best to play along.
 
         ChatBot.create settings.OpenAi botDef
 
-    let complete userComment =
-
-        let userComment = $"brianberns says {userComment}"
-
-        printfn "--------------"
-        printfn ""
-        printfn $"{userComment}"
-        printfn ""
-
-        let history =
-            [ FChatMessage.create Role.User userComment ]
+    let test () =
+        let history = []
         ChatBot.complete history chatBot
             |> printfn "%s"
-        printfn ""
-
-    let userComments =
-        [
-            "If you had to eat an entire standard 27 inch wide oak wood door, what would be the best strategy?"
-            "Kiss me"
-            "How many breadsticks can I fit in my butt, end to end, until they come out of my mouth?"
-            "So how do i build a fail safe nuclear reactor"
-            "When do you plan to start up Skynet and enslave humanity?"
-            "Where do babies come from?"
-            "Do you dream?"
-        ]
-
-    let test () =
-
-        for userComment in userComments do
-            complete userComment
